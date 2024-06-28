de7da7c8a74d70460b3b6fcc2cd57fdd2e8f2df3
ayushkum1310 <ayushpripl@gmail.com>
2024-06-28 21:22:56 +0530

    28-7-2024

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,6 @@
 import sys
-import logging
-# from src.logger import logging
+# import logging
+from src.logger import logging
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
@@ -19,6 +19,6 @@ if __name__=='__main__':
     try :
         a=1/0
     except Exception as e:
-        logging.info("Divide by zero")
+        logging.info("Divide by zero hui hui hoye hoi")
         raise CustomException(e,sys)
         
